add54f0623f8cbbc4952bd3054d269f431687b85
Matthew Pachapa <pachapa36@gmail.com>
2022-05-03 14:15:56 -0400

    kadojf

---
+++ CTAC Exercises/calculations/Calculations.java
@@ -0,0 +1,10 @@
+import java.util.Sanner;
+
+class Calculations {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner();
+        double num1;
+        double num2;
+
+    }
+}
